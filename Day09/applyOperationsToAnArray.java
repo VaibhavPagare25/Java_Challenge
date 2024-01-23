53f3d772989c887802b1e3776c2faae1637a1a1a
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-01-23 22:43:45 +0530

    Second problem solved

---
+++ Day09/applyOperationsToAnArray.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i] = nums[i]*2;
+                nums[i+1] = 0;
+            }
+        }
+
+
+        int zero = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)
+                zero++;
+        }
+        int[] arr = new int[n];
+        int index = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+                arr[index++] = nums[i];
+        }
+
+        for(int i=0;i<zero;i++){
+            arr[index++] = 0;
+        }
+        return arr;
+    }
+}
+
