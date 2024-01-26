c3b8ef53a9094879d32fff04c13e0edb61eaea92
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-01-26 22:39:53 +0530

    First Problem solved

--- Day12/moveZeros.java
+++ Day12/moveZeros.java
@@ -1,18 +1,16 @@
 
 import java.util.*;
-class Solution{
+class Solution {
 
 	int[] moveZeros(int arr[]){
-
+	
 		int count = 0;
 		for(int i = 0; i<arr.length; i++){
-	
 			if(arr[i] != 0){
 				arr[count] = arr[i];
 				count++;
 			}
 		}
-
 		while(count < arr.length){
 			arr[count] = 0;
 			count++;
@@ -26,18 +24,22 @@ class Solution{
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter the size of an Array");
+		System.out.println("Enter the size of an array");
 		int size = sc.nextInt();
-		System.out.println("Enter the elements of an Array");
+
+		System.out.println("Enter the elements of an array");
 		int nums[] = new int[size];
+
 		for(int i = 0; i < size;i++) { 
 
 			nums[i] = sc.nextInt();
 		}
 
 		int[] ret = obj.moveZeros(nums);
+
 		for(int i = 0; i<nums.length; i++){
-			System.out.println(ret[i]);
+
+			System.out.print(ret[i]);
 		}
 	}
 }
