87a956db5e999a28528f08090bbbfed80927e2fa
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-02-28 19:22:18 +0530

    Second problem solved and done for the day

---
+++ Day45/rotateA2DArrayWithoutUsingExtraSpace.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    void rotateMatrix(int arr[][], int n) {
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = arr[i][start];
+                arr[i][start] = arr[i][end];
+                arr[i][end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+}
