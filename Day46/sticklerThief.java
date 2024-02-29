084cd652965daf4cb390bd85d6de872c95d24f07
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-02-29 13:33:22 +0530

    Second problem solved

---
+++ Day46/sticklerThief.java
@@ -0,0 +1,19 @@
+
+class Solution {
+
+    public int FindMaxSum(int arr[], int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], arr[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];
+    }
+}
